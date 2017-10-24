9c3a8239768d226d3cf9337aae817667ec7d889b
orbitfum <orbitfum@gmail.com>
2017-10-24 20:40:47 +0300

    Spartan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel {{$title}}
                 </div>
 
                 <div class="links">
